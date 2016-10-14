1f64ab9755544d9ef00b6fde3e9933b5dadeb529
Victor Fernandes <victor.fernandes@webedia-group.com>
2016-10-14 18:18:09 -0300

    making episode number dinamic

--- js/crawler.js
+++ js/crawler.js
@@ -5,16 +5,18 @@ function getFilm(id){
 
 
 function displayCrawler(film){
-	//console.log(film);
+	console.log(film);
 	parentNode = document.getElementById("crawler");
 	titleNode = document.getElementById("title");
-
+	episodeNode = document.getElementById("episode");
+	episode = document.createTextNode(toRoman(film.episode_id));
 	title = document.createTextNode(film.title);
 	text = document.createTextNode(film.opening_crawl);
 
+	episodeNode.appendChild(episode);
 	titleNode.appendChild(title);
 	parentNode.appendChild(text);
 }
 
 
-getFilm(1);
+getFilm(3);
--- js/helpers.js
+++ js/helpers.js
@@ -12,3 +12,21 @@ function getJson(url, callback){
 		callback(obj);
 	}
 }
+
+
+function toRoman(num) {  
+  var result = '';
+  var decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  var roman = ["M", "CM","D","CD","C", "XC", "L", "XL", "X","IX","V","IV","I"];
+  for (var i = 0;i<=decimal.length;i++) {
+  // looping over every element of our arrays
+    while (num%decimal[i] < num) {   
+    // keep trying the same number until it won't fit anymore      
+      result += roman[i];
+      // add the matching roman number to our result string
+      num -= decimal[i];
+      // remove the decimal value of the roman number from our number
+    }
+  }
+  return result;
+}
